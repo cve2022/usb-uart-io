5474083b40b616bd6bb1cb209f68d2574909870f
cve2022 <cve2022cve@gmail.com>
2022-07-09 18:55:03 -0300

    Initial commit

---
+++ src/tst_uuio.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python3
+
+from usb_uart_io import UsbUartIo
+from usb_uart_io import IN1, IN2, IN3, IN4
+from usb_uart_io import OUT1, OUT2, OUT3
+import time
+
+def main():
+    uio = UsbUartIo()
+    while True:
+        uio.out(OUT1, uio.inp(IN1))
+        uio.out(OUT2, uio.inp(IN2))
+        out3 = uio.inp(IN3) or uio.inp(IN4)
+        uio.out(OUT3, out3)
+        time.sleep(0.2)
+
+if __name__ == "__main__":
+    main()
---
+++ src/usb_uart_io.py
@@ -0,0 +1,41 @@
+#! /usr/bin/env python3
+
+from serial import Serial
+
+IN1, IN2, IN3, IN4 = range(4)
+OUT1, OUT2, OUT3 = range(3)
+ON, OFF = True, False
+
+class UsbUartIo(Serial):
+    def __init__(self, port='/dev/ttyUSB0'):
+        super().__init__(port)
+        self.dtr = False
+        self.rts = False
+    
+    def __del__(self):
+        self.break_condition = False
+        self.dtr = False
+        self.rts = False
+        super().__del__()
+
+    def out(self, id, state):
+        if id == OUT1:
+            self.dtr = state
+        elif id == OUT2:
+            self.rts = state
+        elif id == OUT3:
+            self.break_condition = state
+        else:
+            pass # do nothing
+
+    def inp(self, id):
+        if id == IN1:
+            return self.cts
+        elif id == IN2:
+            return self.dsr
+        elif id == IN3:
+            return self.cd
+        elif id == IN4:
+            return self.ri
+        else:
+            return OFF
